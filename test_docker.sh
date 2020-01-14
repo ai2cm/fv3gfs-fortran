890032db05e002891bd88d83e3c0f3d4fade841a
Jeremy McGibbon <jeremym@vulcan.com>
2020-01-13 16:23:44 -0800

    enable all tests

--- test_docker.sh
+++ test_docker.sh
@@ -9,6 +9,6 @@ export COMPILED_TAG_NAME=$1
 export COMPILED_IMAGE=us.gcr.io/vcm-ml/fv3gfs-compiled-$COMPILED_TAG_NAME
 
 [[ -d inputdata ]] || ./download_inputdata.sh
-# bash tests/test_output_netcdfs_identical.sh &&
-# bash tests/test_regression_fails_for_900s_timestep.sh
+bash tests/test_output_netcdfs_identical.sh &&
+bash tests/test_regression_fails_for_900s_timestep.sh
 TARGET=cpu REAL_TYPE=double COMPILED_IMAGE=$COMPILED_IMAGE bash tests/testsuite.sh
