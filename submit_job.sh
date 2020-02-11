a2030c510013ade290c175b292e3bc5c5d0be704
Jeremy McGibbon <jeremym@vulcan.com>
2020-02-11 15:56:04 -0800

    Feature/unify serialize (#9)
    This PR makes slight changes to how the serialize image is built and tested, to make it more integrated with the build system for the normal image. Specifically:
    
    - Building a serialize image is controlled entirely by whether the `serialize` build_arg is set to `false` (default) or `true`.
    - The serial preprocessing workflow now runs *before* the model is compiled. The compiled model image will use the build_arg to select either the original or preprocessed Fortran sources.
    - Build flags for the serialize model are now set in a new `configure.fv3` file.
    - The compiled image name is now called `us.gcr.io/vcm-ml/fv3gfs-fortran:<version>(-<tag>)`. For example, the latest serialize image is called `us.gcr.io/vcm-ml/fv3gfs-fortran:latest-serialize` and the latest default image is called `us.gcr.io/vcm-ml/fv3gfs-fortran:latest`.
    - The pytest framework is now better parametrized over model images. You can select a particular version to test and it will test both the default and serialize images against the same reference files. We could easily add testing for the debug image in this fashion.
    - Both the serialize image and default image are integration tested on CircleCI.
    - The `make enter` workflow for serialized sources needed to be changed a bit, and has been documented in the README.
    
    Currently there is no command-line control to run only a specific image tag in the regression tests. We could add that later if we want it.
    
    Commit history:
    
    * add clean target to makefile
    
    * working build, build_serialize, and test make targets
    
    * fix update_circleci_reference target in makefile
    
    * build both images on circleci
    
    * fix enter_serialize make target for development workflow and document it in readme
    
    * remove old serialize dockerfile as it has been integrated into core dockerfile
    
    * add docker_buildkit info to readme
    
    * rename source images to clarify relation to target options
    
    * fix copy target for base sources in dockerfile

--- submit_job.sh
+++ submit_job.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-
-HEAD=$1
 RUNDIR=/rundir
 
 ulimit -s unlimited
-cp $HEAD/FV3/fv3.exe $RUNDIR/fv3.exe
+cp /FV3/fv3.exe $RUNDIR/fv3.exe
 cd $RUNDIR
 mpirun -np 6 --allow-run-as-root --mca btl_vader_single_copy_mechanism none --oversubscribe $RUNDIR/fv3.exe
