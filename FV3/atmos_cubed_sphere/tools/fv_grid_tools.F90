d0aa62a9c33597d3d9a34fc9905beab59473d7c9
Rhea George <RheaG@Rheas-MacBook-Pro.local>
2020-05-20 12:01:24 -0700

    save global_grid for Mirror_Grid savepoint after it is reassigned to xs and ys

--- FV3/atmos_cubed_sphere/tools/fv_grid_tools.F90
+++ FV3/atmos_cubed_sphere/tools/fv_grid_tools.F90
@@ -687,7 +687,7 @@ contains
           !$ser savepoint Gnomonic_Grids-Out
           !$ser data lon=xs lat=ys
           !$ser savepoint Mirror_Grid-In
-          !$ser data grid_global=grid_global ng=ng npx=npx npy=npy ndims=ndims_mirror nregions=nregions_mirror
+         
             if (is_master()) then
              if (Atm%flagstruct%grid_type>=0) then
                 do j=1,npy
@@ -696,7 +696,7 @@ contains
                       grid_global(i,j,2,1) = ys(i,j)
                    enddo
                 enddo
-        
+               !$ser data grid_global=grid_global ng=ng npx=npx npy=npy ndims=ndims_mirror nregions=nregions_mirror
 ! mirror_grid assumes that the tile=1 is centered on equator and greenwich meridian Lon[-pi,pi] 
                 call mirror_grid(grid_global, ng, npx, npy, 2, 6)
          !$ser savepoint Mirror_Grid-Out
@@ -750,6 +750,8 @@ contains
                                       n, grid_global(1:npx,1:npy,1,n), grid_global(1:npx,1:npy,2,n))
              enddo
              endif
+           !$ser verbatim else
+             !$ser data grid_global=grid_global ng=ng npx=npx npy=npy ndims=ndims_mirror nregions=nregions_mirror
           endif !is master
           !$ser verbatim   if (.not.is_master()) then
            !$ser savepoint Mirror_Grid-Out
