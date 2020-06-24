7c71e069c164bf890d72a31b722a6517ff9ab18e
Oliver Fuhrer <oli@ginko.ch>
2020-06-24 08:53:32 -0700

    remove in favor of submodule

--- phys_standalone/enter.sh
+++
@@ -1,8 +0,0 @@
-docker stop phys_standalone 1>/dev/null 2>/dev/null
-docker rm phys_standalone 1>/dev/null 2>/dev/null
-docker run -i -t --rm \
-	--mount type=bind,source=`pwd`/turb,target=/work/turb \
-	--mount type=bind,source=`pwd`/seaice,target=/work/seaice \
-	--mount type=bind,source=`pwd`/microph,target=/work/microph \
-	--mount type=bind,source=`pwd`/shalconv,target=/work/shalconv \
-	--name=phys_standalone phys_standalone
