f6405e123a471a0e0f0ba7d436e64d934960449b
Noah D. Brenowitz <nbren12@gmail.com>
2020-07-23 18:51:22 -0700

    Can build dycore lib

--- shell.nix
+++ shell.nix
@@ -2,16 +2,27 @@ let
   pkgs = import <nixpkgs> {};
   fms=  import ./nix/fms;
   nceplibs=  import ./nix/nceplibs;
-      
+  esmf=  import ./nix/esmf;
+  cc = pkgs.gfortran.cc;
 in
-  pkgs.mkShell {
+with import <nixpkgs> {}; {
+  qpidEnv = stdenvNoCC.mkDerivation {
+    name = "my-gcc8-environment";
     buildInputs = [
-      fms nceplibs pkgs.gfortran
+        cc
+        fms
+        esmf
+        nceplibs
+        gfortran
+        netcdffortran
+        openmpi
     ];
 
-
     FMS_DIR="${fms}/include";
-    ESMF_DIR="${fms}";
-    LD_LIBRARY_PATH="$${LD_LIBRARY_PATH}:$${ESMF_DIR}/lib/libO3/Linux.gfortran.64.mpiuni.default/:${fms}/libFMS/.libs/:$${SERIALBOX_DIR}/lib";
-    FFLAGS="-I${fms}/include";
+    ESMF_DIR="${esmf}";
+    #LD_LIBRARY_PATH="$${LD_LIBRARY_PATH}:${esmf}/lib/libO3/Linux.gfortran.64.mpiuni.default/:${fms}/libFMS/.libs/:$${SERIALBOX_DIR}/lib";
+    INCLUDE="-I${fms}/include -I${netcdffortran}/include -I${esmf}/mod/modO3/Linux.gfortran.64.mpiuni.default/";
+    OMPI_CC="${cc}/bin/gcc";
+};
 }
+
