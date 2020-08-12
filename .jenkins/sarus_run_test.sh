23d84e1544e6f924d662ad717bd280b15e479025
Mark Cheeseman <MarkC@Vulcan.com>
2020-08-12 09:53:24 -0700

    Sarus test now uses local examples directory

--- .jenkins/sarus_run_test.sh
+++ .jenkins/sarus_run_test.sh
@@ -13,25 +13,28 @@ python3 -m venv venv
 pip3 install "git+https://github.com/VulcanClimateModeling/fv3config.git"
 
 # Setup the working directory for a C48 FV3GFS run
-cp /project/d107/mcheese/yaml_files/fv3config_c48.yml .
-python create_rundir.py fv3config_c48.yml "./c48_test"
+cd examples
+cp /project/d107/mcheese/yaml_files/fv3config_c48.yml ./fv3config.yml
+python create_rundir.py
 deactivate
 
 # Copy over a SLURM job submission script to run the c48 test run
-cd ${PWD}/c48_test
+#cd ${PWD}/c48_test
+cd rundir
 cp /project/d107/mcheese/jobscripts/job_jenkins_sarus .
 
 # SCRATCH_DIR environment variable is required for the SLURM job to run
-export SCRATCH_DIR=/scratch/snx3000/olifu/jenkins/workspace/fv3gfs/examples/c48_test
+#export SCRATCH_DIR=/scratch/snx3000/olifu/jenkins/workspace/fv3gfs/examples/rundir
+export SCRATCH_DIR=${PWD}
 gcloud auth configure-docker
 
 # Get the dockerfiles for the images to be tested on Piz Daint
-dockerfiles=( $( ls docker/Dockerfile.gnu* ) )
+dockerfiles=( $( ls ../../docker/Dockerfile.gnu* ) )
 
 for df in ${dockerfiles[@]}; do
 
    # Grab the architecture tag from the dockerfile filename
-   arch=${df:18}
+   arch=${df:24}
    export FV3_CONTAINER=fv3gfs-compiled:$arch
    export TAR_FILE=${FV3_CONTAINER/":"/"_"}.tar
 
