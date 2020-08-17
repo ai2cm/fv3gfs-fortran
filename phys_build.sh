24bc56b84dd8c9982278c59646b4c7e005b45507
Andre Perkins <frodre@gmail.com>
2020-08-17 23:17:12 +0000

    Add GCS to docker image to enable download of rundir from k8s

--- phys_build.sh
+++ phys_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-COMPILED_TAG_NAME=physics make build_serialize
+COMPILED_TAG_NAME=physics-andrep make build_serialize
 #DOCKER_BUILDKIT=1 COMPILED_TAG_NAME=physics make build_serialize
 
--- phys_run.sh
+++ phys_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ./rundir; ./clean.sh; cd -
-docker run -it --rm --mount type=bind,source=`pwd`/rundir,target=/rundir us.gcr.io/vcm-ml/fv3gfs-compiled:physics-serialize bash
+docker run -it --rm --mount type=bind,source=`pwd`/rundir,target=/rundir us.gcr.io/vcm-ml/fv3gfs-compiled:physics-andrep-serialize bash
 
