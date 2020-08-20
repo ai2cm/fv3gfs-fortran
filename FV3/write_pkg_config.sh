5890a0c0ae3125f0050a64a2d53d029c069b3ed9
Noah D. Brenowitz <nbren12@gmail.com>
2020-08-19 17:42:14 -0700

    Add nceplibs to pkg-config

--- FV3/write_pkg_config.sh
+++ FV3/write_pkg_config.sh
@@ -6,7 +6,6 @@ NCEPLIBS=$2
 
 cat << EOF
 prefix=${PREFIX}
-nceplibs=${NCEPLIBS}
 exec_prefix=\${prefix}
 includedir=\${prefix}/include
 libdir=\${exec_prefix}/lib
@@ -15,5 +14,5 @@ Name: fv3
 Description: FV3 and Libraries
 Version: 1.0.0
 Cflags: -I\${includedir}
-Libs: -L\${libdir} -lfv3cpl -lfv3 -lipd -lfv3core -lfv3coarse_graining -lfv3io -lgfsphys -lstochastic_physics -L\${nceplibs} -lnemsio_d -lbacio_4 -lsp_v2.0.2_d -lw3emc_d -lw3nco_d
+Libs: -L\${libdir} ${NCEPLIBS} -lfv3cpl -lfv3 -lipd -lfv3core -lfv3coarse_graining -lfv3io -lgfsphys -lstochastic_physics -lFMS -lnetcdff -lnetcdf -llapack -lblas -lesmf 
 EOF
