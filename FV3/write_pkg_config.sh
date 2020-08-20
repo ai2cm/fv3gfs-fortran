69ad5d10bc07863df74338bbc12b3381da9818c9
Noah D. Brenowitz <nbren12@gmail.com>
2020-08-19 17:49:27 -0700

    Pass all ldflags pkgconfig

--- FV3/write_pkg_config.sh
+++ FV3/write_pkg_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PREFIX=$1
-NCEPLIBS=$2
+LDFLAGS=$2
 
 
 cat << EOF
@@ -14,5 +14,5 @@ Name: fv3
 Description: FV3 and Libraries
 Version: 1.0.0
 Cflags: -I\${includedir}
-Libs: -L\${libdir} ${NCEPLIBS} -lfv3cpl -lfv3 -lipd -lfv3core -lfv3coarse_graining -lfv3io -lgfsphys -lstochastic_physics -lFMS -lnetcdff -lnetcdf -llapack -lblas -lesmf 
+Libs: -L\${libdir} ${LDFLAGS} -lfv3cpl -lfv3 -lipd -lfv3core -lfv3coarse_graining -lfv3io -lgfsphys -lstochastic_physics -lFMS -lnetcdff -lnetcdf -llapack -lblas -lesmf 
 EOF
