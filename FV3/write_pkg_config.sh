d762ca50c35c52c4f4229f5865cd0c45407283b1
Noah D. Brenowitz <nbren12@gmail.com>
2020-08-24 18:02:27 -0700

    Correct the missing symbol errors

--- FV3/write_pkg_config.sh
+++ FV3/write_pkg_config.sh
@@ -17,6 +17,6 @@ Name: fv3
 Description: FV3 and Libraries
 Version: 1.0.0
 Cflags: ${CFLAGS} -I${PREFIX}/include
-Libs: -L\${libdir} ${LDFLAGS} -lfv3cpl -lfv3 -lipd -lfv3core -lfv3coarse_graining -lfv3io -lgfsphys -lstochastic_physics -lFMS -lnetcdff -lnetcdf -llapack -lblas -lesmf  -lrt
+Libs: -L\${libdir} ${LDFLAGS}
 EOF
 # librt is needed for ESMF
