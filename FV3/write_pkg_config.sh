fb965e56163acbb0d550fb30fee8ec71eb4a4844
Noah D. Brenowitz <nbren12@gmail.com>
2020-08-24 17:37:58 -0700

    Fix write_pkg_config

--- FV3/write_pkg_config.sh
+++ FV3/write_pkg_config.sh
@@ -2,7 +2,10 @@
 
 PREFIX=$1
 LDFLAGS=$2
+CFLAGS=$3
 
+
+echo $CFLAGS
 
 cat << EOF
 prefix=${PREFIX}
@@ -13,6 +16,7 @@ libdir=\${exec_prefix}/lib
 Name: fv3
 Description: FV3 and Libraries
 Version: 1.0.0
-Cflags: -I\${includedir}
-Libs: -L\${libdir} ${LDFLAGS} -lfv3cpl -lfv3 -lipd -lfv3core -lfv3coarse_graining -lfv3io -lgfsphys -lstochastic_physics -lFMS -lnetcdff -lnetcdf -llapack -lblas -lesmf 
+Cflags: ${CFLAGS} -I${PREFIX}/include
+Libs: -L\${libdir} ${LDFLAGS} -lfv3cpl -lfv3 -lipd -lfv3core -lfv3coarse_graining -lfv3io -lgfsphys -lstochastic_physics -lFMS -lnetcdff -lnetcdf -llapack -lblas -lesmf  -lrt
 EOF
+# librt is needed for ESMF
