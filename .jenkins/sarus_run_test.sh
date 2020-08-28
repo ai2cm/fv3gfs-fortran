d505f51e23f2eabd92840d176ba42c3fee2aa0d6
Mark Cheeseman <MarkC@Vulcan.com>
2020-08-28 09:46:22 -0700

    limit Jenkins c12 test to GNU-9 image

--- .jenkins/sarus_run_test.sh
+++ .jenkins/sarus_run_test.sh
@@ -10,6 +10,7 @@ gcloud auth configure-docker
 
 
 # Set up the working directing for the c12 test
+echo "Setting up working directory"
 cd examples
 . /project/d107/install/venv/sn_1.0/bin/activate
 python write_rundir.py ../tests/pytest/config/default.yml "./c12_test"
@@ -21,23 +22,19 @@ export SCRATCH_DIR=${PWD}
 
 
 # Import the Docker image to be tested on Daint
-df = "Dockerfile.gnu9_mpich314_nocuda"
-arch=${df:24}
-export FV3_CONTAINER=fv3gfs-compiled:$arch
-export TAR_FILE=${FV3_CONTAINER/":"/"_"}.tar
-gsutil copy gs://vcm-ml-public/jenkins-tmp/${TAR_FILE}.gz .
-gunzip ${TAR_FILE}.gz
-sarus load ./${TAR_FILE} ${FV3_CONTAINER}
+echo "Import GNU-9 compiled Docker image into Sarus"
+export FV3_CONTAINER=fv3gfs-compiled:gnu9_mpich314_nocuda
+gsutil copy gs://vcm-ml-public/jenkins-tmp/fv3gfs-compiled_gnu9_mpich314_nocuda.tar.gz .
+gunzip fv3gfs-compiled_gnu9_mpich314_nocuda.tar.gz
+sarus load ./fv3gfs-compiled_gnu9_mpich314_nocuda.tar ${FV3_CONTAINER}
 module unload sarus
 
 
 # run a c12 run for the specified Docker image
+echo "Submitting SLURM c12 run"
 sbatch --wait job_jenkins_sarus
 
 # verify the output files using a md5sum check
+echo "Performing md5sum check of c12 run output"
 md5sum -c ../../tests/pytest/reference/circleci/default/md5.txt
-
-# clean up the working directory for the next run
-rm *.nc RESTART/*.nc
-
 
