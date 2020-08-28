9af592db91df6252f27317619b04fff14151368e
Mark Cheeseman <MarkC@Vulcan.com>
2020-08-27 17:41:01 -0700

    limit m5dsum verification to gnu9-compiled Docker image

--- .jenkins/sarus_run_test.sh
+++ .jenkins/sarus_run_test.sh
@@ -20,30 +20,24 @@ cp ../../.jenkins/job_jenkins_sarus .
 export SCRATCH_DIR=${PWD}
 
 
-# Import the Docker images to be tested on Daint
-dockerfiles=( $( ls ../../docker/Dockerfile.* ) )
-for df in ${dockerfiles[@]}; do
-   arch=${df:24}
-   export FV3_CONTAINER=fv3gfs-compiled:$arch
-   export TAR_FILE=${FV3_CONTAINER/":"/"_"}.tar
-   gsutil copy gs://vcm-ml-public/jenkins-tmp/${TAR_FILE}.gz .
-   gunzip ${TAR_FILE}.gz
-   sarus load ./${TAR_FILE} ${FV3_CONTAINER}
-done
+# Import the Docker image to be tested on Daint
+df = "Dockerfile.gnu9_mpich314_nocuda"
+arch=${df:24}
+export FV3_CONTAINER=fv3gfs-compiled:$arch
+export TAR_FILE=${FV3_CONTAINER/":"/"_"}.tar
+gsutil copy gs://vcm-ml-public/jenkins-tmp/${TAR_FILE}.gz .
+gunzip ${TAR_FILE}.gz
+sarus load ./${TAR_FILE} ${FV3_CONTAINER}
 module unload sarus
 
 
-# Perform a series of runs 
-for df in ${dockerfiles[@]}; do
-   # run a c12 run for the specified Docker image
-   export FV3_CONTAINER=fv3gfs-compiled:${df:24}
-   sbatch --wait job_jenkins_sarus
+# run a c12 run for the specified Docker image
+sbatch --wait job_jenkins_sarus
 
-   # verify the output files using a md5sum check
-   md5sum -c ../../tests/pytest/reference/circleci/default/md5.txt
+# verify the output files using a md5sum check
+md5sum -c ../../tests/pytest/reference/circleci/default/md5.txt
 
-   # clean up the working directory for the next run
-   rm *.nc RESTART/*.nc
-done
+# clean up the working directory for the next run
+rm *.nc RESTART/*.nc
 
 
