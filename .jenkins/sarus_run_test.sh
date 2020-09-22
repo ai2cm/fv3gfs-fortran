7d84fd1907e1e958c18de2dcbf60add39fb53343
Mark Cheeseman <MarkC@Vulcan.com>
2020-09-22 16:07:45 -0700

    remove use of fv3config

--- .jenkins/sarus_run_test.sh
+++ .jenkins/sarus_run_test.sh
@@ -12,8 +12,7 @@ gcloud auth configure-docker
 # Set up the working directing for the c12 test
 cd examples
 . /project/d107/install/venv/sn_1.0/bin/activate
-#python write_rundir.py ../tests/pytest/config/default.yml "./c12_test"
-python -c 'from fv3config import write_run_directory; write_run_directory "../tests/pytest/config/default.yml" "./c12_test"'
+python write_rundir.py ../tests/pytest/config/default.yml "./c12_test"
 deactivate
 
 cd ${PWD}/c12_test
