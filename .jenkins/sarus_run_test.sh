dd0878d6137d92a5fbfafe497f66564887e17642
Mark Cheeseman <MarkC@Vulcan.com>
2020-09-24 08:42:43 -0700

    removed commented lines from sarus_run_test.sh

--- .jenkins/sarus_run_test.sh
+++ .jenkins/sarus_run_test.sh
@@ -54,25 +54,3 @@ for df in ${dockerfiles[@]}; do
    rm RESTART/*.nc
 done
 
-
-#export FV3_CONTAINER=fv3gfs-compiled:gnu9_mpich314_nocuda
-#gsutil copy gs://vcm-jenkins/fv3gfs-compiled_gnu9_mpich314_nocuda.tar.gz .
-#gunzip fv3gfs-compiled_gnu9_mpich314_nocuda.tar.gz
-#sarus load ./fv3gfs-compiled_gnu9_mpich314_nocuda.tar ${FV3_CONTAINER}
-#module unload sarus
-
-#sbatch --wait job_jenkins_sarus
-#md5sum -c ../../tests/pytest/reference/circleci/default/md5.txt
-#rm *.nc 
-#rm RESTART/*.nc
-
-# Run a c12 regression test for the GNU-8 Docker image
-#export FV3_CONTAINER=fv3gfs-compiled:gnu8_mpich314_cuda101
-#gsutil copy gs://vcm-ml-public/jenkins-tmp/fv3gfs-compiled_gnu8_mpich314_cuda101.tar.gz .
-#gunzip fv3gfs-compiled_gnu8_mpich314_cuda101.tar.gz
-#module load sarus
-#sarus load ./fv3gfs-compiled_gnu8_mpich314_cuda101.tar ${FV3_CONTAINER}
-#module unload sarus
-
-#sbatch --wait job_jenkins_sarus
-#md5sum -c ./md5.txt
