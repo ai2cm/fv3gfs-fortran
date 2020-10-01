3f9f452bab472097d40f92d54f1b05571e2d2278
Oliver Fuhrer <oliverf@vulcan.com>
2020-10-01 18:44:00 +0000

    echo stderr/stdout upon error

--- tests/serialized_test_data_generation/submit_job.sh
+++ tests/serialized_test_data_generation/submit_job.sh
@@ -13,13 +13,30 @@ mkdir -p $TEST_DATA_DIR
 
 # setup environment
 
-# run the model (deduce MPI configuration from config file)
+# setup run environment
 cd $RUNDIR
 NUM_RANKS=`python ${RUNDIR}/count_ranks.py $RUNDIR/fv3config.yml`
 ulimit -s unlimited
+
+# run the model (deduce MPI configuration from config file)
+# note: we want to catch errors, hence the set +e/-e logic
 echo ">>> Running model, inspect stdout.out / stderr.out in rundir for details"
+set +e
 mpirun -l -np $NUM_RANKS $RUNDIR/fv3.exe 2>$RUNDIR/stderr.out 1>$RUNDIR/stdout.out
-echo ">>> Success"
+if [ $? -ne 0 ] ; then
+    echo ">>> Error occurred while running the model"
+    echo ">>>>> cmd:"
+    echo "mpirun -l -np $NUM_RANKS $RUNDIR/fv3.exe"
+    echo ">>>>> stderr:"
+    cat $RUNDIR/stderr.out
+    echo ">>>>> stdout:"
+    cat $RUNDIR/stdout.out
+    echo ">>>>> Aborting"
+    exit 1
+else
+    echo ">>> Success"
+fi
+set -e
 
 # copy artefacts to test_data directory
 cp $RUNDIR/input.nml $TEST_DATA_DIR/
