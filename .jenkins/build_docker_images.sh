4c529c5450816e21e787c354ea34afe5147644b6
Mark Cheeseman <MarkC@vulcan.com>
2020-10-01 22:14:11 +0000

    updated build script in Jenkins plan

--- .jenkins/build_docker_images.sh
+++ .jenkins/build_docker_images.sh
@@ -4,14 +4,17 @@
 export DOCKER_BUILDKIT=1
 export BUILDKIT_PROGRESS=plain
 targets="build build_serialize_gt4py_dev"
-tags="hpc hpc-serialize"
+tags="gnu9-mpich314-nocuda gnu9-mpich314-nocuda-serialize"
 
 # Speed-up the compilations by using pre-built MPI, FMS, and ESMF images
 export BUILD_FROM_INTERMEDIATE=y
 
+# We build and test Docker images without CUDA support
+export CUDA=n
+
 # Build FV3 without and with Serialbox support enabled
 make pull_deps
-COMPILED_TAG_NAME=hpc make ${targets} 
+make ${targets} 
 
 # For each newly built Docker image:
 #   - push image to VCM's Google Container Repository (necessary?)
