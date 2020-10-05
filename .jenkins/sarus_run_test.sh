e03a07c9922f0a5aa874cb7d338ba60e1359076e
Oliver Fuhrer <ofuhrer@users.noreply.github.com>
2020-10-05 09:57:30 -0700

    Update sarus_run_test.sh (#77)

--- .jenkins/sarus_run_test.sh
+++ .jenkins/sarus_run_test.sh
@@ -6,7 +6,7 @@ tags="hpc hpc-serialize"
 
 # Set up the compute node environment
 module load daint-gpu
-module add /project/d107/install/modulefiles/
+module add /project/s1053/install/modulefiles/
 module load cray-python gcloud 
 gcloud auth configure-docker
 
@@ -15,7 +15,7 @@ gcloud auth configure-docker
 # Using the standard python virtual environment for Piz Daint
 # defined in https://github.com/VulcanClimateModeling/daint_venv
 cd examples
-. /project/d107/install/venv/sn_1.0/bin/activate
+. /project/s1053/install/venv/sn_1.0/bin/activate
 pip install -r ../requirements.txt
 python -c 'import fv3config; import yaml; fid=open("../tests/pytest/config/default.yml", "r"); config = yaml.safe_load(fid); fv3config.write_run_directory(config, "./c12_test")'
 deactivate
