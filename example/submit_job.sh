f869a4810dbe7189b679a59f0535b3d5bf642976
Spencer Clark <spencerkclark@gmail.com>
2020-10-15 08:33:18 -0400

    Update example/submit_job.sh for MPICH (#88)

--- example/submit_job.sh
+++ example/submit_job.sh
@@ -5,4 +5,4 @@ RUNDIR=/rundir
 ulimit -s unlimited
 cp /FV3/fv3.exe $RUNDIR/fv3.exe
 cd $RUNDIR
-mpirun -np 6 --allow-run-as-root --mca btl_vader_single_copy_mechanism none --oversubscribe $RUNDIR/fv3.exe
+mpirun -np 6 $RUNDIR/fv3.exe
