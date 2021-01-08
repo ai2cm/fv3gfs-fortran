bdf990f2fb693949c67f74f8eaf9ed95aa477c76
Oliver Fuhrer <oli@ginko.ch>
2021-01-07 17:41:17 -0800

    updates & fixes

--- coverage.sh
+++ coverage.sh
@@ -26,6 +26,12 @@ submit_job=${base_dir}/tests/serialized_test_data_generation/submit_job.sh
 count_ranks=${base_dir}/tests/serialized_test_data_generation/count_ranks.py
 gcr_url="us.gcr.io/vcm-ml/fv3gfs-compiled"
 
+# don't overwrite any previous coverage data
+if [ -d ${coverage_dir} ] ; then
+    echo "The directory ${coverage_dir} already exists. Remove and try again."
+    exit 1
+fi
+
 # build docker container with model compiled for coverage
 COMPILED_TAG_NAME=gcov COMPILED_IMAGE=${gcr_url}:gcov COMPILE_OPTION="OPENMP=\\\nREPRO=\\\nDEBUG=Y\\\nGCOV=Y\\\nGT4PY_DEV=Y" make -C "${base_dir}" build
 
@@ -37,7 +43,7 @@ cp "${count_ranks}" "${rundir}"
 
 # run the model
 mkdir -p ${coverage_dir}
-docker run -it --rm --network host -v ${coverage_dir}:/coverage -v `pwd`/rundir:/rundir ${gcr_url}:gcov bash -c "cd /rundir; ./submit_job.sh; pip install gcovr; cd /coverage; mkdir physics; cd physics; gcovr -d -r /FV3/gfsphysics --html --html-details -o index.html; cd ../; mkdir dycore; cd dycore; gcovr -d -r /FV3/atmos_cubed_sphere --html --html-details -o index.html"
+docker run -it --rm --network host -v ${coverage_dir}:/coverage -v `pwd`/rundir:/rundir ${gcr_url}:gcov bash -c "set -ex; cd /rundir; ./submit_job.sh; pip3 install gcovr; cd /coverage; mkdir physics; cd physics; gcovr -d -r /FV3/gfsphysics --html --html-details -o index.html; cd ../; mkdir dycore; cd dycore; gcovr -d -r /FV3/atmos_cubed_sphere --html --html-details -o index.html"
 
 # cleanup run directory
 if grep Termination ${rundir}/stdout.out ; then
@@ -47,5 +53,5 @@ else
 fi
 
 # tell user we're done
-echo "Open ${config_name}/index.html to view coverage data"
+echo "Open coverage_${config_name}/*/index.html to view coverage data"
 exit 0
