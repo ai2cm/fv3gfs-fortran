f0781420522bd4ecd3d4d72fc7892311e66f454d
Oliver Fuhrer <oliver.fuhrer@meteoswiss.ch>
2021-01-22 06:26:11 +0100

    add other namelists

--- benchmarking/dsl_single_node/run_all.sh
+++ benchmarking/dsl_single_node/run_all.sh
@@ -5,8 +5,7 @@ workdir=/scratch/snx3000/olifu/benchmarking
 partition="normal --no-wait"
 #partition="debug"
 
-#for config in c96 c128 c192 c256 ; do
-for config in c128 ; do
+for config in c96 c128 c192 c256 ; do
 for compiler in gnu intel ; do
 
   exe=/project/s1053/install/fv3gfs-fortran/${compiler}/fv3_64bit.exe
@@ -38,8 +37,6 @@ for compiler in gnu intel ; do
   ./run_benchmark.py --no-hyperthreading --threads_per_rank=4 --nodes_per_tile_side=1 --rank_layout=2 ${default_args}/${config}_${compiler}_no_4_3x1
   ./run_benchmark.py --hyperthreading    --threads_per_rank=8 --nodes_per_tile_side=1 --rank_layout=1 ${default_args}/${config}_${compiler}_yes_8_1x3
   ./run_benchmark.py --hyperthreading    --threads_per_rank=8 --nodes_per_tile_side=1 --rank_layout=2 ${default_args}/${config}_${compiler}_yes_8_3x1
-
-  exit 0
 
 done
 done
