802dd4148f119bad01711e25b6e71a604cd34876
Oliver Fuhrer <oli@ginko.ch>
2021-03-05 15:39:55 -0800

    update

--- phys_standalone/build.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -d serialbox ]; then
-  git clone -b master https://github.com/VulcanClimateModeling/serialbox2.git serialbox
-  cd serialbox
-  git pull
-  git checkout savepoint_as_string
-  cd -
-fi
-
-docker build -t phys_standalone .
