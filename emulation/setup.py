d18e696709c56d86dcc1705f46aa43f70b0533e2
Andre Perkins <frodre@gmail.com>
2021-08-12 22:36:09 +0000

    Fix numpy/tflow version conflicts

--- emulation/setup.py
+++ emulation/setup.py
@@ -6,10 +6,10 @@
 from setuptools import setup, find_packages
 
 requirements = [
-    "numpy>=1.11",
+    "numpy>=1.15",
     "fsspec>=0.6.2",
     "pyyaml>=5.1.2",
-    "tensorflow==2.3.0",
+    "tensorflow==2.5.0",
     "mpi4py>=3.0.3",
     "cftime",
     "f90nml",
