c7d112a609ca1c9964008ff141ddb697d2bf679a
Andre Perkins <frodre@gmail.com>
2021-09-08 00:22:13 +0000

    Add prognostic hooks and module

--- FV3/gfsphysics/GFS_layer/GFS_physics_driver.F90
+++ FV3/gfsphysics/GFS_layer/GFS_physics_driver.F90
@@ -4568,6 +4568,21 @@ module module_physics_driver
             call set_state("tendency_of_rain_water_mixing_ratio_due_to_microphysics", rainp)
             call call_function("emulation.monitor", "store_zarr")
             call call_function("emulation.monitor", "store_netcdf")
+            
+            ! apply microphysics emulator
+            call call_function("emulation.emulate", "microphysics")
+
+            call get_state("air_temperature_output", Stateout%gt0)
+            call get_state("specific_humidity_output", qv_post_precpd)
+            call get_state("cloud_water_mixing_ratio_output", qc_post_precpd)
+            call get_state("total_precipitation", rain1)
+
+            do k=1,levs
+              do i=1,im
+                Stateout%gq0(i,k,1) = qv_post_precpd(i,k)
+                Stateout%gq0(i,k,ntcw) = qc_post_precpd(i,k)
+              enddo
+            enddo
 #endif
             
           endif
---
+++ emulation/emulation/emulate.py
@@ -0,0 +1,78 @@
+import f90nml
+import logging
+import os
+import tensorflow as tf
+
+from .debug import print_errors
+from ._filesystem import get_dir
+from .monitor import store_zarr
+
+logger = logging.getLogger(__name__)
+
+TF_MODEL_PATH = None  # local or remote path to tensorflow model
+STORE_EMU_DATA = None
+NML_PATH = None
+DT_SEC = None
+
+@print_errors
+def _load_environment_vars_into_global():
+
+    global TF_MODEL_PATH
+    global STORE_EMU_DATA
+    global NML_PATH
+
+    cwd = os.getcwd()
+    TF_MODEL_PATH = os.environ["TF_MODEL_PATH"]
+    STORE_EMU_DATA = bool(os.environ.get("STORE_EMU_DATA", False))
+    NML_PATH = os.path.join(cwd, "input.nml")
+
+
+@print_errors
+def _load_nml():
+    namelist = f90nml.read(NML_PATH)
+    logger.info(f"Loaded namelist for emulation from {NML_PATH}")
+    
+    return namelist
+
+@print_errors
+def _get_timestep(namelist):
+    return int(namelist["coupler_nml"]["dt_atmos"])
+
+
+@print_errors
+def _load_tf_model() -> tf.keras.Model:
+    logger.debug(f"Loading keras model: {TF_MODEL_PATH}")
+    with get_dir(TF_MODEL_PATH) as local_model_path:
+        model = tf.keras.models.load_model(local_model_path)
+
+    return model
+
+
+_load_environment_vars_into_global()
+NML = _load_nml()
+DT_SEC = _get_timestep(NML)
+MODEL = _load_tf_model()
+
+
+@print_errors
+def microphysics(state):
+    global USE_PHYS_COUNTER
+
+    inputs = [state[name].T for name in MODEL.input_names]
+    predictions = MODEL.predict(inputs)
+    model_outputs = {
+        name: output.T # transposed adjust
+        for name, output in zip(MODEL.output_names, predictions)
+    }
+
+    overwrites = set(state).intersection(model_outputs)
+    logger.info(f"Overwritting existing state fields: {overwrites}")
+    microphysics_diag = {
+        f"{orig_updated}_physics_diag": state[orig_updated]
+        for orig_updated in overwrites
+    }
+    state.update(model_outputs)
+    state.update(microphysics_diag)
+
+    if STORE_EMU_DATA:
+        store_zarr(state)
