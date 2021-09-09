9c4e574fed1f6e269e0420239b39fc265ddb668f
Andre Perkins <frodre@gmail.com>
2021-09-09 18:51:10 +0000

    Add dummy model for testing

---
+++ emulation/test_model/create_test_model.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+from pathlib import Path
+
+def create_model():
+    in_ = tf.keras.layers.Input(shape=(63,), name="air_temperature_input")
+    out_ = tf.keras.layers.Lambda(
+        lambda x: x, 
+        output_shape=(63,),
+        name="air_temperature_dummy"
+    )(in_)
+    model = tf.keras.Model(inputs=in_, outputs=out_)
+
+    return model
+
+
+if __name__ == "__main__":
+
+    model = create_model()
+    path = Path(__file__).parent / "dummy_model.tf"
+    model.save(path.as_posix(), save_format="tf")
+
