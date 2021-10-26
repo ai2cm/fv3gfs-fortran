86967edd88f7213a189ebfd4cc7af5a396d7bfc6
oelbert <olivere@allenai.org>
2021-10-26 14:39:18 -0400

    reworking trig metric terms again...

--- FV3/atmos_cubed_sphere/model/fv_grid_utils.F90
+++ FV3/atmos_cubed_sphere/model/fv_grid_utils.F90
@@ -374,8 +374,8 @@
      !$ser savepoint UtilVectors-Out
      !$ser data ec1=ec1 ec2=ec2 ew1=ew(:,:,:,1) ew2=ew(:,:,:,2) es1=es(:,:,:,1) es2=es(:,:,:,2)
 
-     !$ser savepoint TrigTerms-In
-     !$ser data grid=grid agrid=agrid cos_sg1=cos_sg(:,:,1) sin_sg1=sin_sg(:,:,1) cos_sg2=cos_sg(:,:,2) sin_sg2=sin_sg(:,:,2) cos_sg3=cos_sg(:,:,3) sin_sg3=sin_sg(:,:,3) cos_sg4=cos_sg(:,:,4) sin_sg4=sin_sg(:,:,4) cos_sg5=cos_sg(:,:,5) sin_sg5=sin_sg(:,:,5) cos_sg6=cos_sg(:,:,6) sin_sg6=sin_sg(:,:,6) cos_sg7=cos_sg(:,:,7) sin_sg7=sin_sg(:,:,7) cos_sg8=cos_sg(:,:,8) sin_sg8=sin_sg(:,:,8) cos_sg9=cos_sg(:,:,9) sin_sg9=sin_sg(:,:,9) ec1=ec1 ec2=ec2 ee1=ee1 ee2=ee2 cosa_u=cosa_u cosa_v=cosa_v cosa_s=cosa_s sina_u=sina_u sina_v=sina_v cosa=cosa sina=sina rsin_u=rsin_u rsin_v=rsin_v rsina=rsina rsin2=rsin2
+     !$ser savepoint TrigSg-In
+     !$ser data grid=grid agrid=agrid cos_sg1=cos_sg(:,:,1) sin_sg1=sin_sg(:,:,1) cos_sg2=cos_sg(:,:,2) sin_sg2=sin_sg(:,:,2) cos_sg3=cos_sg(:,:,3) sin_sg3=sin_sg(:,:,3) cos_sg4=cos_sg(:,:,4) sin_sg4=sin_sg(:,:,4) cos_sg5=cos_sg(:,:,5) sin_sg5=sin_sg(:,:,5) cos_sg6=cos_sg(:,:,6) sin_sg6=sin_sg(:,:,6) cos_sg7=cos_sg(:,:,7) sin_sg7=sin_sg(:,:,7) cos_sg8=cos_sg(:,:,8) sin_sg8=sin_sg(:,:,8) cos_sg9=cos_sg(:,:,9) sin_sg9=sin_sg(:,:,9) ec1=ec1 ec2=ec2
      
 !     9---4---8
 !     |       |
@@ -515,7 +515,7 @@
   endif !if (grid_type < 3)
 
    if ( non_ortho ) then
-      !$ser savepoint TrigSubset-In
+      !$ser savepoint DerivedTrig-In
       !$ser data grid=grid cos_sg1=cos_sg(:,:,1) sin_sg1=sin_sg(:,:,1) cos_sg2=cos_sg(:,:,2) sin_sg2=sin_sg(:,:,2) cos_sg3=cos_sg(:,:,3) sin_sg3=sin_sg(:,:,3) cos_sg4=cos_sg(:,:,4) sin_sg4=sin_sg(:,:,4) cos_sg5=cos_sg(:,:,5) sin_sg5=sin_sg(:,:,5) cos_sg6=cos_sg(:,:,6) sin_sg6=sin_sg(:,:,6) cos_sg7=cos_sg(:,:,7) sin_sg7=sin_sg(:,:,7) cos_sg8=cos_sg(:,:,8) sin_sg8=sin_sg(:,:,8) cos_sg9=cos_sg(:,:,9) sin_sg9=sin_sg(:,:,9) ee1=ee1 ee2=ee2 cosa_u=cosa_u cosa_v=cosa_v cosa_s=cosa_s sina_u=sina_u sina_v=sina_v cosa=cosa sina=sina rsin_u=rsin_u rsin_v=rsin_v rsina=rsina rsin2=rsin2
            cosa_u = big_number
            cosa_v = big_number
@@ -630,7 +630,7 @@
             endif
          enddo
       enddo
-      !$ser savepoint TrigSubset-Out
+      !$ser savepoint DerivedTrig-Out
       !$ser data ee1=ee1 ee2=ee2 cosa_u=cosa_u cosa_v=cosa_v cosa_s=cosa_s sina_u=sina_u sina_v=sina_v cosa=cosa sina=sina rsin_u=rsin_u rsin_v=rsin_v rsina=rsina rsin2=rsin2
       
       !EXPLANATION HERE: calling fill_ghost overwrites **SOME** of the sin_sg 
@@ -704,8 +704,8 @@
 !!!      cos_sg(npx,npy,6) = 0.5*(cos_sg(npx-1,npy,7)+cos_sg(npx,npy-1,9))
       endif
       
-      !$ser savepoint TrigTerms-Out
-      !$ser data cos_sg1=cos_sg(:,:,1) sin_sg1=sin_sg(:,:,1) cos_sg2=cos_sg(:,:,2) sin_sg2=sin_sg(:,:,2) cos_sg3=cos_sg(:,:,3) sin_sg3=sin_sg(:,:,3) cos_sg4=cos_sg(:,:,4) sin_sg4=sin_sg(:,:,4) cos_sg5=cos_sg(:,:,5) sin_sg5=sin_sg(:,:,5) cos_sg6=cos_sg(:,:,6) sin_sg6=sin_sg(:,:,6) cos_sg7=cos_sg(:,:,7) sin_sg7=sin_sg(:,:,7) cos_sg8=cos_sg(:,:,8) sin_sg8=sin_sg(:,:,8) cos_sg9=cos_sg(:,:,9) sin_sg9=sin_sg(:,:,9) ee1=ee1 ee2=ee2 cosa_u=cosa_u cosa_v=cosa_v cosa_s=cosa_s sina_u=sina_u sina_v=sina_v cosa=cosa sina=sina rsin_u=rsin_u rsin_v=rsin_v rsina=rsina rsin2=rsin2
+      !$ser savepoint TrigSg-Out
+      !$ser data cos_sg1=cos_sg(:,:,1) sin_sg1=sin_sg(:,:,1) cos_sg2=cos_sg(:,:,2) sin_sg2=sin_sg(:,:,2) cos_sg3=cos_sg(:,:,3) sin_sg3=sin_sg(:,:,3) cos_sg4=cos_sg(:,:,4) sin_sg4=sin_sg(:,:,4) cos_sg5=cos_sg(:,:,5) sin_sg5=sin_sg(:,:,5) cos_sg6=cos_sg(:,:,6) sin_sg6=sin_sg(:,:,6) cos_sg7=cos_sg(:,:,7) sin_sg7=sin_sg(:,:,7) cos_sg8=cos_sg(:,:,8) sin_sg8=sin_sg(:,:,8) cos_sg9=cos_sg(:,:,9) sin_sg9=sin_sg(:,:,9)
    else !if non_ortho
            sina = 1.
            cosa = 0.
