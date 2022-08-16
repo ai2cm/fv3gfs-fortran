61675f18facd1c6b5a97979427f071ddaa5db7c2
YakelynRJ <yakelynr@uw.edu>
2022-08-16 21:40:22 +0000

    replace get_data.py

--- FV3/wrapper/run_model.sh
+++ FV3/wrapper/run_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf rundirectory
 write_run_directory fv3config.yml rundirectory
-cp get_data.py rundirectory/
+cp validate_radiation.py  rundirectory/
 cd rundirectory
 mpirun -n 6 python validate_radiation.py 
 
