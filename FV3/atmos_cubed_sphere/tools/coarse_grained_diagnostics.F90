c899d34f930c00ef16e2955cbb21900df265d517
Spencer Clark <spencerkclark@gmail.com>
2023-02-10 19:17:11 +0000

    Add coarse-grained vorticity diagnostics

--- FV3/atmos_cubed_sphere/tools/coarse_grained_diagnostics.F90
+++ FV3/atmos_cubed_sphere/tools/coarse_grained_diagnostics.F90
@@ -4,7 +4,7 @@ module coarse_grained_diagnostics_mod
   use diag_manager_mod, only: diag_axis_init, register_diag_field, register_static_field, send_data
   use field_manager_mod,  only: MODEL_ATMOS
   use fv_arrays_mod, only: fv_atmos_type, fv_coarse_graining_type
-  use fv_diagnostics_mod, only: cs3_interpolator, get_height_field, get_height_given_pressure
+  use fv_diagnostics_mod, only: cs3_interpolator, get_height_field, get_height_given_pressure, get_vorticity, interpolate_vertical
   use fv_mapz_mod, only: moist_cp, moist_cv
   use mpp_domains_mod, only: domain2d, EAST, NORTH
   use mpp_mod, only: FATAL, mpp_error
@@ -125,6 +125,15 @@ contains
     coarse_diagnostics(index)%reduction_method = AREA_WEIGHTED
     coarse_diagnostics(index)%data%var2 => Atm(tile_count)%ps(is:ie,js:je)
     
+    index = index + 1
+    coarse_diagnostics(index)%axes = 3
+    coarse_diagnostics(index)%module_name = DYNAMICS
+    coarse_diagnostics(index)%name = 'vort_coarse'
+    coarse_diagnostics(index)%description = 'coarse-grained relative vorticity'
+    coarse_diagnostics(index)%units = '1/s'
+    coarse_diagnostics(index)%reduction_method = MASS_WEIGHTED
+    coarse_diagnostics(index)%special_case = 'vorticity'
+
     if (.not. Atm(tile_count)%flagstruct%hydrostatic) then
        index = index + 1
        coarse_diagnostics(index)%axes = 3
@@ -507,6 +516,16 @@ contains
       coarse_diagnostics(index)%reduction_method = AREA_WEIGHTED
       coarse_diagnostics(index)%special_case = 'height'
 
+      index = index + 1
+      coarse_diagnostics(index)%pressure_level = pressure_levels(p)
+      coarse_diagnostics(index)%axes = 2
+      coarse_diagnostics(index)%module_name = DYNAMICS
+      coarse_diagnostics(index)%name = 'vort' // trim(adjustl(pressure_level_label)) // '_coarse'
+      coarse_diagnostics(index)%description = 'coarse-grained ' // trim(adjustl(pressure_level_label)) // '-mb vorticity'
+      coarse_diagnostics(index)%units = '1/s'
+      coarse_diagnostics(index)%reduction_method = AREA_WEIGHTED
+      coarse_diagnostics(index)%special_case = 'vorticity'
+
       do t = 1, n_tracers
         call get_tracer_names(MODEL_ATMOS, t, tracer_name, tracer_long_name, tracer_units)
         index = index + 1
@@ -797,7 +816,9 @@ contains
     real, allocatable :: work_2d(:,:), work_2d_coarse(:,:), work_3d_coarse(:,:,:)
     real, allocatable :: mass(:,:,:), height_on_interfaces(:,:,:), masked_area(:,:,:)
     real, allocatable :: phalf(:,:,:), upsampled_coarse_phalf(:,:,:)
+    real, allocatable, target :: vorticity(:,:,:)
     integer :: is, ie, js, je, is_coarse, ie_coarse, js_coarse, je_coarse, npz
+    integer :: isd, ied, jsd, jed
     logical :: used
     logical :: need_2d_work_array, need_3d_work_array, need_mass_array, need_height_array, need_masked_area_array
     integer :: index, i, j
@@ -814,6 +835,10 @@ contains
       need_masked_area_array = .false.
     endif
 
+    isd = Atm(tile_count)%bd%isd
+    ied = Atm(tile_count)%bd%ied
+    jsd = Atm(tile_count)%bd%jsd
+    jed = Atm(tile_count)%bd%jed
     call get_fine_array_bounds(is, ie, js, je)
     call get_coarse_array_bounds(is_coarse, ie_coarse, js_coarse, je_coarse)
     npz = Atm(tile_count)%npz
@@ -865,6 +890,13 @@ contains
       if (.not. allocated(work_2d_coarse)) allocate(work_2d_coarse(is_coarse:ie_coarse,js_coarse:je_coarse))
       allocate(work_2d(is:ie,js:je))
     endif
+
+    if (need_vorticity_array()) then
+      allocate(vorticity(is:ie,js:je,1:npz))
+      call get_vorticity(is, ie, js, je, isd, ied, jsd, jed, npz, Atm(tile_count)%u, Atm(tile_count)%v, vorticity, &
+              Atm(tile_count)%gridstruct%dx, Atm(tile_count)%gridstruct%dy, Atm(tile_count)%gridstruct%rarea)
+      call associate_vorticity_pointers(is, ie, js, je, npz, vorticity)
+   endif
 
     do index = 1, DIAG_SIZE
       if (coarse_diagnostics(index)%id .gt. 0) then
@@ -1021,6 +1053,25 @@ contains
           height_on_interfaces(is:ie,js:je,1:npz+1), &
           Atm%peln(is:ie,1:npz+1,js:je), &
           coarse_diag%pressure_level, &
+          work_2d(is:ie,js:je) &
+        )
+        call weighted_block_average( &
+          Atm%gridstruct%area(is:ie,js:je), &
+          work_2d, &
+          result &
+          )
+      elseif (trim(coarse_diag%special_case) .eq. 'vorticity') then
+        ! Note that FV3 uses a different interpolation routine for vorticity
+        ! than other vertically interpolated diagnostics.
+        call interpolate_vertical( &
+          is, &
+          ie, &
+          js, &
+          je, &
+          npz, &
+          100.0 * coarse_diag%pressure_level, &  ! Convert mb to Pa
+          Atm%peln(is:ie,1:npz+1,js:je), &
+          coarse_diag%data%var3, &
           work_2d(is:ie,js:je) &
         )
         call weighted_block_average( &
@@ -1484,4 +1535,31 @@ contains
     endif
     result = sum(total * delp, dim=3) / grav
   end subroutine compute_multi_species_water_path
+
+  logical function need_vorticity_array()
+    integer :: index
+
+    need_vorticity_array = .false.
+    do index = 1, DIAG_SIZE
+      if (trim(coarse_diagnostics(index)%special_case) .eq. 'vorticity' .and. &
+          coarse_diagnostics(index)%id .gt. 0) then
+          need_vorticity_array = .true.
+          exit
+      endif
+    enddo
+  end function need_vorticity_array
+
+  subroutine associate_vorticity_pointers(is, ie, js, je, npz, vorticity)
+    integer, intent(in) :: is, ie, js, je, npz
+    real, target, intent(in) :: vorticity(is:ie,js:je,1:npz)
+ 
+     integer :: index
+ 
+     do index = 1, DIAG_SIZE
+       if (trim(coarse_diagnostics(index)%special_case) .eq. 'vorticity' .and. &
+           coarse_diagnostics(index)%id .gt. 0) then
+           coarse_diagnostics(index)%data%var3 => vorticity(is:ie,js:je,1:npz)
+       endif
+     enddo
+  end subroutine associate_vorticity_pointers
 end module coarse_grained_diagnostics_mod
